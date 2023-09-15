81ecfefbef57d64afbf1b13aa6f196e7737f72ad
akebu6 <asimasiku999@gmail.com>
2023-09-15 13:39:50 +0200

    task: updated code_files
    closes #14

--- code_files/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import navbar, home, footer
-
-urlpatterns = [
-    path('', navbar, name='navbar',),
-    path('', home, name='home'),
-    path('', footer, name='footer')
-]
