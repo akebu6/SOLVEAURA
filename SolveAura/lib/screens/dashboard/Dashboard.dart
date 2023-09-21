94fdab78715893103149170e50a27cf65e232831
akebu6 <asimasiku999@gmail.com>
2023-09-21 21:27:51 +0200

    chore: base layout

--- SolveAura/lib/screens/dashboard/Dashboard.dart
+++ SolveAura/lib/screens/dashboard/Dashboard.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Dashboard extends StatefulWidget {
   static const routeName = '/dashboard';
@@ -11,6 +11,10 @@ class Dashboard extends StatefulWidget {
 class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(
+        child: Text('Dashboard'),
+      ),
+    );
   }
 }
