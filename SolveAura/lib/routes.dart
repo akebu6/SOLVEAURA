99b5274155b56c6ab26632b7f7880200adadd7c2
akebu6 <asimasiku999@gmail.com>
2023-09-21 20:55:57 +0200

    chore: updated code files

---
+++ SolveAura/lib/routes.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:solveaura/screens/dashboard/Dashboard.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  Dashboard.routeName: (_) => const Dashboard(),
+};
---
+++ SolveAura/lib/screens/dashboard/Dashboard.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class Dashboard extends StatefulWidget {
+  static const routeName = '/dashboard';
+  const Dashboard({super.key});
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
