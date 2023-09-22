34776aa7787b8ce85bc5256baac5213bea8ea754
akebu6 <asimasiku999@gmail.com>
2023-09-23 00:29:41 +0200

    chore: reset files

--- SolveAura/lib/ui/pages/home.dart
+++ SolveAura/lib/ui/pages/home.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
   static const routeName = '/home';
-  const Homed({super.key});
+  const Home({super.key});
 
   @override
   State<Home> createState() => _HomeState();
 }
 
-class _HomeState extends State<Dashboard> {
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,7 +44,7 @@ class BottomNavigation extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Dashboard(),
+      home: Home(),
     );
   }
 }
