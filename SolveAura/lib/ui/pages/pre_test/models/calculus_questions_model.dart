9db760d265368e00ed33e3abce86eac06cbcd733
akebu6 <asimasiku999@gmail.com>
2023-11-19 18:15:05 +0200

    task: calculus pretest
    Added calculus pretest questions that will also be used to check whether or not the user has understood what they were learning

--- SolveAura/lib/ui/pages/pre_test/models/calculus_questions_model.dart
+++ SolveAura/lib/ui/pages/pre_test/models/calculus_questions_model.dart
@@ -46,104 +46,55 @@ class CalculusOption {
 
 final calculusQuestionsList = [
   CalculusQuestion(
-    text: "What is the value of x in the given equation: 4x + 10 = 30?",
+    text: "What is the derivative of the function f(x) = 4cos2x + logx + x?",
     options: [
-      const CalculusOption(text: "5", isCorrect: true),
-      const CalculusOption(text: "4", isCorrect: false),
-      const CalculusOption(text: "2", isCorrect: false),
+      const CalculusOption(text: "8sin2x+1/x+x", isCorrect: false),
+      const CalculusOption(text: "−8sin2x+1/x+x", isCorrect: false),
+      const CalculusOption(text: "−8sin2x+1/x+1", isCorrect: true),
       const CalculusOption(text: "1", isCorrect: false),
     ],
     id: 0,
     correctAnswer:
-    const CalculusOption(text: "5", isCorrect: true),
+    const CalculusOption(text: "−8sin2x+1/x+1", isCorrect: true),
   ),
   CalculusQuestion(
     text:
-    "How many types of methods are there to solve simultaneous algebraic equations?",
+    "What is the integral of the function f(x) = sin 2x?",
     options: [
-      const CalculusOption(text: "2", isCorrect: true),
-      const CalculusOption(text: "3", isCorrect: false),
-      const CalculusOption(text: "4", isCorrect: false),
-      const CalculusOption(text: "1", isCorrect: false),
+      const CalculusOption(text: "(1/2) sin x + C", isCorrect: false),
+      const CalculusOption(text: "(-1/2) cos x + C", isCorrect: false),
+      const CalculusOption(text: "(-1/2) cos 2x + C", isCorrect: true),
+      const CalculusOption(text: "(1/2) sin 2x + C", isCorrect: false),
     ],
     id: 1,
-    correctAnswer: const CalculusOption(text: "2", isCorrect: true),
+    correctAnswer: const CalculusOption(text: "(-1/2) cos 2x + C", isCorrect: true),
   ),
 
   CalculusQuestion(
     text:
-    "The two equations that have no values to satisfy both equations then this is called?",
+    "Integral of the constant function f(x) = k is:",
     options: [
-      const CalculusOption(text: "Consistent system ", isCorrect: false),
-      const CalculusOption(text: "Constant system", isCorrect: false),
-      const CalculusOption(text: "Inconsistent system", isCorrect: true),
-      const CalculusOption(text: "Solution system", isCorrect: false),
+      const CalculusOption(text: "C", isCorrect: false),
+      const CalculusOption(text: "0", isCorrect: false),
+      const CalculusOption(text: "k+C", isCorrect: false),
+      const CalculusOption(text: "kx+C", isCorrect: false),
     ],
     id: 2,
-    correctAnswer: const CalculusOption(text: "Inconsistent system", isCorrect: true),
+    correctAnswer: const CalculusOption(text: "kx+C", isCorrect: true),
   ),
 
   CalculusQuestion(
     text:
-    "If A = 2x - y + 3xy, B = x + 2xy and C = 3y + xy, find the value of A+B+C",
+    "Evaluate the approximate value of the definite integral of ln(x) over [2,4]",
     options: [
-      const CalculusOption(text: "3xy–2y–6xy", isCorrect: false),
-      const CalculusOption(text: "5xy-2y", isCorrect: false),
-      const CalculusOption(text: "–3xy+2y+6xy", isCorrect: false),
-      const CalculusOption(text: "3x+2y+6xy", isCorrect:true),
+      const CalculusOption(text: "-6.3", isCorrect: false),
+      const CalculusOption(text: "2.16", isCorrect: true),
+      const CalculusOption(text: "-1.204", isCorrect: false),
+      const CalculusOption(text: "0.9", isCorrect: false),
     ],
     id: 3,
     correctAnswer:
-    const CalculusOption(text: "3x+2y+6xy ", isCorrect: true),
+    const CalculusOption(text: "2.16 ", isCorrect: true),
   ),
   // other 4
-  CalculusQuestion(
-    text:
-    "A chocolate bar costs c pence and a drink costs d pence. Write down an expression for the cost of 2 chocolate bars and 2 drinks.",
-    options: [
-      const CalculusOption(text: "c + d", isCorrect: false),
-      const CalculusOption(text: "2c + d", isCorrect: false),
-      const CalculusOption(text: "2c + 2d", isCorrect: true),
-      const CalculusOption(text: "2c - 2d", isCorrect: false),
-    ],
-    id: 4,
-    correctAnswer: const CalculusOption(text: "2c + 2d", isCorrect: true),
-  ),
-  CalculusQuestion(
-    text: "I'm a horizontal version of Column. Who am I?",
-    options: [
-      const CalculusOption(text: "Row ", isCorrect: true),
-      const CalculusOption(text: "Divider", isCorrect: false),
-      const CalculusOption(text: "Column", isCorrect: false),
-      const CalculusOption(text: "Stack", isCorrect: false),
-    ],
-    id: 5,
-    correctAnswer: const CalculusOption(text: "Row", isCorrect: true),
-  ),
-
-  CalculusQuestion(
-    text:
-    "I align widgets to the top, bottom, center inside a Column. What am I?",
-    options: [
-      const CalculusOption(text: "Row", isCorrect: false),
-      const CalculusOption(text: "Align", isCorrect: false),
-      const CalculusOption(text: "Spacer", isCorrect: false),
-      const CalculusOption(text: "MainAxisAlignment ", isCorrect: true),
-    ],
-    id: 6,
-    correctAnswer:
-    const CalculusOption(text: "MainAxisAlignment ", isCorrect: true),
-  ),
-  CalculusQuestion(
-    text:
-    "I align my Row or Column children differently based on available space. Who am I?",
-    options: [
-      const CalculusOption(text: "Expanded", isCorrect: false),
-      const CalculusOption(text: "Flex ", isCorrect: true),
-      const CalculusOption(text: "FittedBox", isCorrect: false),
-      const CalculusOption(text: "Wrap", isCorrect: false),
-    ],
-    id: 7,
-    correctAnswer: const CalculusOption(text: "Scoped Model", isCorrect: true),
-  ),
 ];
