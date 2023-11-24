47597a76cdd65a12d1cb76f790deeee5e5079543
akebu6 <asimasiku999@gmail.com>
2023-11-25 01:41:10 +0200

    task: tables
    Added the tables with the equation's values

--- SolveAura/lib/ui/pages/learn.dart
+++ SolveAura/lib/ui/pages/learn.dart
@@ -148,6 +148,215 @@ class _LearnState extends State<Learn> {
                           ),
                         ),
                        ),
+                      Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Text(
+                          "This is read, ‘‘The limit of f(x) as x approaches c is L.",
+                          style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                            fontSize: 15,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10),
+                        child:  Text(
+                          "To illustrate, let us consider",
+                          style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                            fontSize: 15,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: TeXView(
+                          renderingEngine: TeXViewRenderingEngine.mathjax(),
+                          child: TeXViewDocument(
+                            r"""
+                            \lim_{x\to 2} \frac{x^2-4}{x-2}
+                            """,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Text(
+                          "Here, f(x)=1+3x and the constant c, which x will approach, is 2. To evaluate the given limit, we will make use of a table to help us keep track of the effect that the approach of x toward 2 will have on f(x). Of course, on the number line, x may approach 2 in two ways: through values on its left and through values on its right. We first consider approaching 2 from its left or through values less than 2. Remember that the values to be chosen should be close to 2",
+                          style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                            fontSize: 15,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Table(
+                          border: TableBorder.all(),
+                           columnWidths: const <int, TableColumnWidth>{
+                             0: FractionColumnWidth(0.25),
+                             1: FractionColumnWidth(0.25),
+                           },
+                          children: [
+                            TableRow(
+                              children: [
+                                Text("x"),
+                                Text("f(x)"),
+                              ],
+                            ),
+                            TableRow(
+                                children: [
+                                  Text("1"),
+                                  Text("4"),
+                                ],
+                            ),
+                            TableRow(
+                                children: [
+                                  Text("1.4"),
+                                  Text("5.2"),
+                                ],
+                            ),
+                            TableRow(
+                                children: [
+                                  Text("1.7"),
+                                  Text("6.1"),
+                                ],
+                            ),
+                            TableRow(
+                                children: [
+                                  Text("1.9"),
+                                  Text("6.7"),
+                                ],
+                            ),
+                            TableRow(
+                                children: [
+                                  Text("1.95"),
+                                  Text("6.85"),
+                                ],
+                            ),
+                            TableRow(
+                              children: [
+                                Text("1.997"),
+                                Text("6.991"),
+                              ],
+                            ),
+                            TableRow(
+                              children: [
+                                Text("1.9999"),
+                                Text("6.9997"),
+                              ],
+                            ),
+                            TableRow(
+                              children: [
+                                Text("1.9999999"),
+                                Text("6.9999997"),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Text(
+                          "Now we consider approaching 2 from its right or through values greater than but close to 2.",
+                          style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                            fontSize: 15,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Table(
+                          border: TableBorder.all(),
+                          columnWidths: const <int, TableColumnWidth>{
+                            0: FractionColumnWidth(0.25),
+                            1: FractionColumnWidth(0.25),
+                          },
+                          children: [
+                            TableRow(
+                              children: [
+                                Text("x"),
+                                Text("f(x)"),
+                              ],
+                            ),
+                            TableRow(
+                              children: [
+                                Text("3"),
+                                Text("10"),
+                              ],
+                            ),
+                            TableRow(
+                              children: [
+                                Text("2.5"),
+                                Text("8.5"),
+                              ],
+                            ),
+                            TableRow(
+                              children: [
+                                Text("2.2"),
+                                Text("7`"),
+                              ],
+                            ),
+                            TableRow(
+                              children: [
+                                Text("2.1"),
+                                Text("7.3"),
+                              ],
+                            ),
+                            TableRow(
+                              children: [
+                                Text("2.03"),
+                                Text("7.09"),
+                              ],
+                            ),
+                            TableRow(
+                              children: [
+                                Text("2.009"),
+                                Text("7.027"),
+                              ],
+                            ),
+                            TableRow(
+                              children: [
+                                Text("2.0005"),
+                                Text("7.0015"),
+                              ],
+                            ),
+                            TableRow(
+                              children: [
+                                Text("2.0000001"),
+                                Text("7.0000003"),
+                              ],
+                            ),
+                          ],
+                        )
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Text(
+                          "Observe that as the values of x get closer and closer to 2, the values of f(x) get closer and closer to 7. This behavior can be shown no matter what set of values, or what direction, is taken in approaching 2. In symbols,",
+                          style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                            fontSize: 15,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: TeXView(
+                          renderingEngine: TeXViewRenderingEngine.mathjax(),
+                          child: TeXViewDocument(
+                            r"""
+                            \lim_{x\to 2} \frac{x^2-4}{x-2} = 7
+                            """,
+                          ),
+                        ),
+                      ),
                     ],
                   ),
                 ),
